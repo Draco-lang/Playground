99e57b3ac0de53655623c8dc2048e418838fe1f3
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-27 20:53:13 +0200

    Added monaco

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,8 @@
+import { Editor } from "@monaco-editor/react";
 
 function App() {
   return (
-    <div>
-      <header>
-        <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-        <a href="https://reactjs.org">Learn React</a>
-      </header>
-    </div>
+    <Editor height={600} />
   );
 }
 
