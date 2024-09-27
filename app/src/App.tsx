1f1f1d5977ff50f0edc70dfd79439dacaa9a962c
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-27 20:49:16 +0200

    Added empty react project

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+        <a href="https://reactjs.org">Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
