33aab9a2ee317f9c95926a3d5d4de25f0c948afc
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-28 09:36:00 +0200

    Added flexlayout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,52 @@
+import {Layout, Model, TabNode, IJsonModel} from 'flexlayout-react';
 import { Editor } from "@monaco-editor/react";
+import 'flexlayout-react/style/light.css';
+
+var layoutDescription : IJsonModel= {
+  global: {},
+  borders: [],
+  layout: {
+      type: "row",
+      weight: 100,
+      children: [
+          {
+              type: "tabset",
+              weight: 50,
+              children: [
+                  {
+                      type: "tab",
+                      name: "One",
+                      component: "editor",
+                  }
+              ]
+          },
+          {
+              type: "tabset",
+              weight: 50,
+              children: [
+                  {
+                      type: "tab",
+                      name: "Two",
+                      component: "editor",
+                  }
+              ]
+          }
+      ]
+  }
+};
+
+const layoutModel = Model.fromJson(layoutDescription);
 
 function App() {
+  const componentFactory = (node: TabNode) => {
+    var component = node.getComponent();
+    if (component === "editor") {
+      return <Editor height={600} />;
+    }
+  }
+
   return (
-    <Editor height={600} />
+    <Layout model={layoutModel} factory={componentFactory} />
   );
 }
 
